1ec722b1580798f73bd1eae26effd17076a55e43
User <piyadakh@nvc.ac.th>
2021-01-10 14:48:49 +0700

    Lab3

---
+++ src/LAB3/ex_for.java
@@ -0,0 +1,13 @@
+package LAB3;
+
+public class ex_for {
+    public static void main(String[] args){
+        //1.พิมพ์ข้อความ Hello 10 ครั้ง
+        for (int i = 0; i <10; i++) {
+            System.out.println("Hello");
+        }
+        System.out.println("Good Bye");
+
+
+    }//main
+}//class
