da1bc4f03190616203b7964abd2a7e732f88810f
LabComMT-3 <piyadakh@nvc.ac.th>
2021-02-16 11:39:02 +0700

    LAB8

--- src/LAB8/CompanyApp.java
+++ src/LAB8/CompanyApp.java
@@ -1,22 +1,50 @@
 package LAB8;
 
+import LAB7.Car;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CompanyApp {
     public static void main(String[] args) {
 
         Employee emp1 = new Employee();
 
-        emp1.setPid("1809900840125");
-        emp1.setName("Piyada Khunpibun");
-        emp1.setAge(23);
-        emp1.setGender("Female");
-        emp1.setTel("0897457455");
-
-        emp1.setEmpID("emp001");
-        emp1.setPosition("Programmer");
-        emp1.setSalary(30000.00);
+        ArrayList<Employee> myEmployee = new ArrayList<Employee>();
+        for (int i = 0; i < 3; i++){
+            //input data to object
+            System.out.println("Please, enter Employee info "+(i+1)+":");
+            Employee e = inputDataObject();
+            myEmployee.add(e);
+            displayObject(myEmployee);
+        }
 
         //display object data
         System.out.println(emp1.toString());
 
+
     }//main
+
+    private static Employee inputDataObject() {
+        Scanner sc = new Scanner(System.in);
+
+        Employee e = new Employee();
+        System.out.print("Enter Employee emp: ");
+        String id = sc.nextLine();
+        e.setEmpID(id);
+
+        System.out.print("Enter Employee Position: ");
+        e.setPosition(sc.nextLine());
+
+        System.out.print("Enter Employee Salary: ");
+        e.setSalary(sc.nextInt());
+        return e;
+    }
+
+    private static void displayObject(ArrayList<Employee> myEmployee) {
+        System.out.println("All objects in Arraylist");
+        for (Employee e:myEmployee){
+            System.out.println(e.toString());
+        }
+    }
 }
